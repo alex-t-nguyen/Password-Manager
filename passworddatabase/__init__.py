405ba6b2c46b75c836d4f67f4a1dfcc9f9a065bb
Alex Nguyen <alexnguyen543@gmail.com>
2020-08-05 16:50:29 -0500

    Removed debug=True from run.py

--- passworddatabase/__init__.py
+++ passworddatabase/__init__.py
@@ -9,12 +9,10 @@ app.config['SECRET_KEY'] = '10bf65da7e684075001d3f07f2b9ca43'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///password_manager.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
 
 from passworddatabase import routes
