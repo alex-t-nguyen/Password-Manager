3df8388300587981ee7159facdf0c5271391a169
Alex Nguyen <alexnguyen543@gmail.com>
2020-08-05 15:51:52 -0500

    Added sidebar elements and changed delete account button placement

--- passworddatabase/forms.py
+++ passworddatabase/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from passworddatabase.models import User
 
@@ -15,12 +15,12 @@ class RegistrationForm(FlaskForm):
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-        if user:    # If user is none -> don't throw error, else username is taken & throw error
+        if user:  # If user is none -> don't throw error, else username is taken & throw error
             raise ValidationError('Username already exists. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
-        if user:    # If user is none -> don't throw error, else username is taken & throw error
+        if user:  # If user is none -> don't throw error, else username is taken & throw error
             raise ValidationError('Email already exists. Please choose a different one.')
 
 
@@ -36,17 +36,18 @@ class UpdateAccountForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
+    delete = BooleanField(default='Delete')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
-            if user:    # If user is none -> don't throw error, else username is taken & throw error
+            if user:  # If user is none -> don't throw error, else username is taken & throw error
                 raise ValidationError('Username already exists. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
-            if user:    # If user is none -> don't throw error, else username is taken & throw error
+            if user:  # If user is none -> don't throw error, else username is taken & throw error
                 raise ValidationError('Email already exists. Please choose a different one.')
 
 
--- passworddatabase/routes.py
+++ passworddatabase/routes.py
@@ -17,13 +17,15 @@ db.create_all()  # Create table in database
 @app.route("/")
 @app.route("/home")
 def home():
+    num_passwords = sidebar_num_passwords()
+    num_password_duplicates = sidebar_num_pass_duplicates()
     if current_user.is_authenticated:
         first_password = Service.query.filter_by(owner=current_user).first()
         if first_password is not None:
             page = request.args.get('page', 1, type=int)
             service_pass = Service.query.order_by(Service.date_created.desc()).paginate(page=page, per_page=5)
-            return render_template('home.html', posts=service_pass)
-    return render_template('home.html', posts=None)
+            return render_template('home.html', posts=service_pass, num_passwords=num_passwords, num_password_duplicates=num_password_duplicates, user=current_user.username)
+    return render_template('home.html', posts=None, num_passwords=num_passwords, num_password_duplicates=num_password_duplicates, user=None)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -81,7 +83,7 @@ def save_picture(form_picture):
 @login_required
 def account():
     form = UpdateAccountForm()
-    if form.validate_on_submit():
+    if form.submit.data and form.validate():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
@@ -90,6 +92,10 @@ def account():
         db.session.commit()
         flash('Account has been updated', 'success')
         return redirect(url_for('account'))
+    # if form.delete.data and form.validate():
+    # delete_account(current_user.id)
+    # return redirect(url_for('home'))
+
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
@@ -152,6 +158,32 @@ def delete_account(user_id):
 
     flash(f'Account has been deleted.', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/read_me")
+def read_me():
+    return render_template('read_me.html', title='READ ME')
+
+
+def sidebar_num_passwords():
+    if current_user.is_authenticated:
+        password_list_size = Service.query.filter_by(owner=current_user).count()
+        return password_list_size
+    else:
+        return 0
+
+
+def sidebar_num_pass_duplicates():
+    num_duplicates = 0
+    if current_user.is_authenticated:
+        service_list = Service.query.filter_by(owner=current_user).all()
+        set_of_services = set()
+        for service in service_list:
+            if service.password in set_of_services:
+                num_duplicates += 1
+            else:
+                set_of_services.add(service.password)
+    return num_duplicates
 
 
 # Password Generation Functions
